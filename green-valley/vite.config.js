0b223f8dcbe5b9d104b65bde4216131aee1735c6
RR RootRise Digital Innovations <rrrootrisedigitalinnovations@gmail.com>
2026-08-11 09:54:36 +0530

    Set GitHub Pages base path

--- green-valley/vite.config.js
+++ green-valley/vite.config.js
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/green-valley-international-school/",
+
   plugins: [react()],
+
   build: {
     target: "es2020",
     sourcemap: false,
